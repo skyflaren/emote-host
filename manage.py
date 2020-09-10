52cdc5295ebbad5c24925d69719bbffb9c1b2dcd
Justin Lu <wabasabi359@gmail.com>
2020-09-10 13:26:57 -0400

    set up basic functionality

---
+++ manage.py
@@ -0,0 +1,16 @@
+from flask import Flask, redirect, url_for, render_template, request, send_from_directory
+
+app = Flask(__name__, static_url_path='')
+
+
+@app.route("/")
+def home():
+	return send_from_directory('img', 'poggies')
+
+@app.route('/<path>')
+def fetch(path):
+	path += ".png"
+	return send_from_directory('img', path)
+
+if __name__ == "__main__":
+	app.run(debug=True)
