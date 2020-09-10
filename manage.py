1973af3934076103c0083c4728cc57d498c6c5a2
Justin Lu <wabasabi359@gmail.com>
2020-09-10 17:34:29 -0400

    style the default index page

--- manage.py
+++ manage.py
@@ -5,13 +5,13 @@ app = Flask(__name__, static_url_path='')
 
 @app.route("/")
 def home():
-	return send_from_directory('img', 'poggies.png')
+	return render_template("index.html")
 
 @app.route('/<path>')
 def fetch(path):
-	try: return send_from_directory('img', path+'.png')
+	try: return send_from_directory("/static/img", path+".png")
 	except:
-		try: return send_from_directory('img', path+'.gif')
+		try: return send_from_directory("/static/img", path+".gif")
 		except:
 			try: return redirect("https://aaerialys.cf/emotes/"+path+".png")
 			except: return redirect("https://aaerialys.cf/emotes/"+path+".gif")
