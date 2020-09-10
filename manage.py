6d6be4028f6d7d65d2a148b7705c8d4146164a8a
dulldesk <54601393+dulldesk@users.noreply.github.com>
2020-09-10 17:05:19 -0400

    Use requests instead of redirect

--- manage.py
+++ manage.py
@@ -1,7 +1,10 @@
-from flask import Flask, redirect, url_for, render_template, request, send_from_directory
+from flask import Flask, redirect, url_for, render_template, request, send_from_directory, abort, make_response
+import requests
 
 app = Flask(__name__, static_url_path='')
 
+extensions = ['png','gif']
+urls = ['https://aaerialys.cf/emotes']
 
 @app.route("/")
 def home():
@@ -9,12 +12,17 @@ def home():
 
 @app.route('/<path>')
 def fetch(path):
-	try: return send_from_directory('img', path+'.png')
-	except:
-		try: return send_from_directory('img', path+'.gif')
-		except:
-			try: return redirect("https://aaerialys.cf/emotes/"+path+".png")
-			except: return redirect("https://aaerialys.cf/emotes/"+path+".gif")
+	for link in urls:
+		for ext in extensions:
+			try: 
+				resp = requests.get("%s/%s.%s" % (link, path, ext))
+				if resp.status_code >= 400: raise Exception
+				return (resp.content, resp.status_code, resp.headers.items())
+			except: continue
+	for ext in extensions:
+		try: return send_from_directory('img', '%s.%s' % (path, ext))
+		except: continue
+	return abort(404)
 
 if __name__ == "__main__":
 	app.run(debug=True)
